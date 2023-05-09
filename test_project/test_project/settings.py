8acffa86ff603892555c5ca017c6f633424cc55d
Kseniy Yakovlev <kseniy.yakovlev@mail.ru>
2023-05-09 14:52:52 +0300

    Rest 2.0

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5e&d&o04$ulotirw9r4b5de#=-2c41xpwunhjx#v3$#d$qldel
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["kseniyyakovlev.pythonanywhere.com"]
+ALLOWED_HOSTS = ["kseniyyakovlev.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'lists',
 ]
 
